94d0c4fe6cc486ef4280fc0eb57f8846ae255bb0
Edward Shields <edward.brendan.shields@cern.ch>
2020-07-29 11:14:53 +0100

    Large overhaul of mixing class

--- AmpGen/Mixing.h
+++ AmpGen/Mixing.h
@@ -4,47 +4,46 @@
 #include <stddef.h>
 #include <complex>
 
-#include "AmpGen/CoherentSum.h"
 #include "AmpGen/Event.h"
 #include "AmpGen/EventList.h"
 #include "AmpGen/EventType.h"
 #include "AmpGen/Types.h"
 #include "AmpGen/MinuitParameterSet.h"
-
+#include "AmpGen/AddCPConjugate.h"
+#include "AmpGen/CoherentSum.h"
 
 namespace AmpGen
 {
-  class Mixing
+
+  class Mixing : public CoherentSum
   {
   public:
     Mixing();
-    Mixing( const EventType& type, const AmpGen::MinuitParameterSet& mps );
-
+    Mixing( const EventType& type, const MinuitParameterSet& mps, const std::string& prefix = "" );
     ~Mixing() {};
 
     complex_t gp( const real_t& t ) const;
     complex_t gm( const real_t& t ) const;
 
-    real_t prob( const Event& evt )              const;
     real_t prob_unnormalised( const Event& evt ) const;
+
+    complex_t getVal   ( const Event& evt ) const;
+    complex_t getValDir( const Event& evt ) const;
+    complex_t getValCnj( const Event& evt ) const;
 
     void setX( const real_t& x )    { _x = x; }
     void setY( const real_t& y )    { _y = y; }
     void setZ( const complex_t& z ) { _z = z; }
 
-    // PDF methods.
-    void prepare();
-    void reset( bool resetEvents = false );
-    void setEvents( EventList& list );
-
-
   private:
-  CoherentSum pdf_dir;
-  CoherentSum pdf_cnj;
+    size_t           m_sizeDir;
+    size_t           m_sizeCnj;
+    EventType        m_evtTypeCnj;
+    AmplitudeRules   m_rulesCnj;
 
-  real_t _x = {0.004};
-  real_t _y = {0.006};
-  complex_t _z = complex_t(_x, _y);
+    real_t    _x = {0.004};
+    real_t    _y = {0.006};
+    complex_t _z = complex_t(_x, _y);
   };
 } // namespace AmpGen
 #endif
--- src/Mixing.cpp
+++ src/Mixing.cpp
@@ -1,20 +1,45 @@
 #include "AmpGen/Mixing.h"
 
+#include "AmpGen/CompilerWrapper.h"
+#include "AmpGen/ThreadPool.h"
 
 using namespace AmpGen;
 
-Mixing::Mixing(const EventType& type, const AmpGen::MinuitParameterSet& mps)
+Mixing::Mixing( const EventType& type, const MinuitParameterSet& mps, const std::string& prefix  )
+  : CoherentSum  ( true, type, mps, prefix )
+  , m_evtTypeCnj (type.conj(1,0)) // Conjugate of D0.
 {
-  // Define PDF.
-  INFO( type );
-  pdf_dir = CoherentSum( type, mps );
-
-  // Define conj. PDF.
-  EventType eventType = type.conj();
-  INFO( typeCnj );
-  MinuitParameterSet mpsCnj( mps )
-  AddCPConjugate(mpsCnj);
-  pdf_cnj = CoherentSum( typeCnj, mps );
+  auto amplitudes      = m_rules.getMatchingRules( m_evtType, prefix);
+  if( amplitudes.size() == 0 ){
+    WARNING("The defined amplitudes don't seem to be able to be able to generate eventType: " << type);
+  }
+  auto amplitudesCnj  = m_rules.getMatchingRules( m_evtTypeCnj, prefix );
+  if ( amplitudes.size() == 0 ){
+    WARNING("The define amplitudes don't seem to be able to be able to generate eventType: " << type.conj(1,0));
+  }
+  m_sizeDir = amplitudes.size();
+  for (auto& amp : amplitudesCnj ) amplitudes.push_back( amp );
+  for( auto& amp : amplitudes )    INFO( amp.first.decayDescriptor() );
+  // for( auto& amp : amplitudesCnj ) INFO( amp.first.decayDescriptor() );
+  m_matrixElements.resize( amplitudes.size() );
+  m_normalisations.resize( m_matrixElements.size(), m_matrixElements.size() );
+  size_t      nThreads = NamedParameter<size_t>     ("nCores"    , std::thread::hardware_concurrency(), "Number of threads to use" );
+  ThreadPool tp(nThreads);
+  for(size_t i = 0; i < m_matrixElements.size(); ++i){
+    if ( i < amplitudes.size() ) {
+      tp.enqueue( [i,this,&mps,&amplitudes]{ 
+      m_matrixElements[i] = TransitionMatrix<complex_t>( amplitudes[i].first,    amplitudes[i].second,    mps, this->m_evtType.getEventFormat(),    this->m_dbThis);
+      CompilerWrapper().compile( m_matrixElements[i].amp, this->m_objCache); 
+      } );
+    }
+    else {
+      tp.enqueue( [i,this,&mps,&amplitudesCnj]{ 
+      m_matrixElements[i] = TransitionMatrix<complex_t>( amplitudesCnj[i].first, amplitudesCnj[i].second, mps, this->m_evtTypeCnj.getEventFormat(), this->m_dbThis);
+      CompilerWrapper().compile( m_matrixElements[i].amp, this->m_objCache); 
+      } );
+    }
+  }
+  m_isConstant = false;
 }
 
 complex_t Mixing::gp(const real_t& t) const
@@ -29,25 +54,38 @@ complex_t Mixing::gm(const real_t& t) const
 
 real_t Mixing::prob_unnormalised( const Event& evt ) const
 {
-  return std::norm( _pdf_dir.getVal( evt ) * gp( evt[ evt.size() -1 ] ) + _pdf_cnj.getVal( evt ) * gm( evt[ evt.size() -1 ] ) );
+  // | A |^2 = | g+( t )*A + q/p*g-( t )*Abar |^2
+  return std::norm( gp( evt[evt.size()-1] )*getValDir( evt ) + gm( evt[evt.size()-1] )*getValCnj( evt ) );
 }
 
-void Mixing::prepare()
+complex_t Mixing::getVal( const Event& evt ) const
 {
-  pdf_dir.prepare();
-  pdf_cnj.prepare();
+  complex_t value( 0., 0. ), dir( 0., 0. ), cnj( 0., 0. );
+  for ( size_t i = 0; i < m_sizeDir; ++i )
+    dir += m_matrixElements[i].coefficient * evt.getCache( m_matrixElements[i].addressData );
+  for ( size_t i = m_sizeDir; i < m_matrixElements.size(); i++ )
+    cnj += m_matrixElements[i].coefficient * evt.getCache( m_matrixElements[i].addressData );
+  
+  value = dir + cnj;
+  return value;
 }
 
-void Mixing::reset( bool resetEvents )
+complex_t Mixing::getValDir( const Event& evt ) const
 {
-  pdf_dir.reset();
-  pdf_cnj.reset();
+  complex_t value( 0., 0. );
+  for ( size_t i = 0; i < m_sizeDir; ++i )
+    value += m_matrixElements[i].coefficient * evt.getCache( m_matrixElements[i].addressData );
+  return value;
 }
 
-void Mixing::setEvents( EventList& list )
+complex_t Mixing::getValCnj( const Event& evt ) const
 {
-  pdf_dir.setEvents( list );
-  pdf_cnj.setEvents( list );
+  complex_t value( 0., 0. );
+  for ( size_t i = m_sizeDir; i < m_matrixElements.size(); ++i )
+    value += m_matrixElements[i].coefficient * evt.getCache( m_matrixElements[i].addressData );
+  return value;
 }
+
+
 
 
